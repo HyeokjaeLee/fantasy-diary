f444d0d89bedbf96740ca3dbbcfa28a388a168b4
HyeokjaeLee <leehyeokjae97@gmail.com>
2025-08-29 00:34:13 +0900

    chore: supabase code 디렉토리 정리

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from '@hey-api/openapi-ts';
 export default defineConfig({
   input: './__generated__/supabase/openapi.json',
   output: './__generated__/supabase',
+  plugins: ['@hey-api/client-next', 'zod'],
 });
